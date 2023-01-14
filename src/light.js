02d8e5b231d943bd449d5507719e0eacdc1ab0ba
Noah De Marco <noahdicampolieto@gmail.com>
2023-01-14 16:09:46 +0100

    Add files via upload

---
+++ src/light.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { ReactDOM } from "react";
+import "./light.css";
+
+
+
+
+const Light =()=>{
+
+
+    return (
+        <>
+
+
+          <style>
+            {`
+              body {
+                background-color: rgb(240,240,240);
+              }
+            `}
+          </style>
+
+
+
+
+
+       
+
+
+
+        
+
+      </>
+    );
+
+
+};
+export default Light;
